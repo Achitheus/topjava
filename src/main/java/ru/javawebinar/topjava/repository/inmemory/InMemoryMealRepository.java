785ef2d8054851ddea3441ee90370cc8054c8e52
Yuri <yur_yurch@mail.ru>
2022-10-31 05:12:52 +0300

    HW2 is done

--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,46 +1,57 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
+import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 
+@Repository
 public class InMemoryMealRepository implements MealRepository {
-    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private final Map<Integer, Map<Integer, Meal>> repository = new ConcurrentHashMap<>();
     private final AtomicInteger counter = new AtomicInteger(0);
 
     {
-        MealsUtil.meals.forEach(this::save);
+        MealsUtil.meals.forEach(meal -> save(meal, 1));
     }
 
-    @Override
-    public Meal save(Meal meal) {
+    @Override // null if updated meal does not belong to userId
+    public Meal save(Meal meal, int userId) {
+        Map<Integer, Meal> usersMap = repository.computeIfAbsent(userId, ConcurrentHashMap::new);
         if (meal.isNew()) {
             meal.setId(counter.incrementAndGet());
-            repository.put(meal.getId(), meal);
+            usersMap.put(meal.getId(), meal);
             return meal;
         }
         // handle case: update, but not present in storage
-        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+        return usersMap.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
     }
 
-    @Override
-    public boolean delete(int id) {
-        return repository.remove(id) != null;
+    @Override // false if meal does not belong to userId
+    public boolean delete(int id, int userId) {
+        Map<Integer, Meal> usersMap = repository.get(userId);
+        return usersMap != null && usersMap.remove(id) != null;
     }
 
-    @Override
-    public Meal get(int id) {
-        return repository.get(id);
+    @Override // null if meal does not belong to userId
+    public Meal get(int id, int userId) {
+        Map<Integer, Meal> usersMap = repository.get(userId);
+        return (usersMap == null) ? null : usersMap.get(id);
     }
 
-    @Override
-    public Collection<Meal> getAll() {
-        return repository.values();
+    @Override // ORDERED dateTime desc
+    public Collection<Meal> getAll(int userId) {
+        Map<Integer, Meal> usersMap = repository.get(userId);
+        return usersMap.isEmpty() ? Collections.emptyList() : usersMap.values().stream()
+                .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+                .collect(Collectors.toList());
     }
 }
 
--- src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/MealsUtil.java
@@ -1,7 +1,7 @@
 package ru.javawebinar.topjava.util;
 
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.MealTo;
+import ru.javawebinar.topjava.to.MealTo;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -33,6 +33,13 @@ public class MealsUtil {
 
     public static List<MealTo> getFilteredTos(Collection<Meal> meals, int caloriesPerDay, LocalTime startTime, LocalTime endTime) {
         return filterByPredicate(meals, caloriesPerDay, meal -> DateTimeUtil.isBetweenHalfOpen(meal.getTime(), startTime, endTime));
+    }
+
+    public static List<MealTo> getFilteredTos(Collection<Meal> meals, int caloriesPerDay, LocalTime startTime, LocalTime endTime, LocalDate startDate, LocalDate endDate) {
+        Predicate<Meal> filter = meal1 -> (DateTimeUtil.isBetweenClosed(meal1.getDate(), startDate, endDate)
+                && DateTimeUtil.isBetweenHalfOpen(meal1.getTime(), startTime, endTime)
+        );
+        return filterByPredicate(meals, caloriesPerDay, filter);
     }
 
     private static List<MealTo> filterByPredicate(Collection<Meal> meals, int caloriesPerDay, Predicate<Meal> filter) {
