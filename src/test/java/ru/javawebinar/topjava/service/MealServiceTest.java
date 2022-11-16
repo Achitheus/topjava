dba0ea82dfb2756789e849947908aa7b4f698515
Yuri <yur_yurch@mail.ru>
2022-11-16 17:16:01 +0300

    HW3 is done

---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,48 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
+import sun.nio.cs.UTF_8;
+
+import static org.junit.Assert.*;
+
+@ContextConfiguration({"classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF_8"))
+public class MealServiceTest {
+    @Autowired
+    MealService mealService;
+
+
+    @Test
+    public void get() {
+        Meal meal = mealService.get()
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getBetweenInclusive() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void create() {
+    }
+}
